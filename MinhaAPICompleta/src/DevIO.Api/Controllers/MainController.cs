7d2e94dd3fe82bb3965f2a4497a623cfe5b8e784
Alan Vieira Martins <alanhorcrux@gmail.com>
2020-01-26 20:32:54 -0300

    Setup de criação da controller Fornecedor e startup do projeto com acesso ao banco

---
+++ MinhaAPICompleta/src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // validacao de notificacao de erro
+
+        // validacao de modelstate
+
+        // validacao de operacao de negocios
+    }
+}
